dae6c8c2450e80069373b7b257fb6f5062ef55c8
Vinay Tiparadi <vinaytiparadi123@gmail.com>
2023-04-13 12:34:35 +0530

    determine specialist based on the disease predicted

--- main.py
+++ main.py
@@ -38,12 +38,72 @@ def predict(s):
     svm_prediction = data_dict["predictions_classes"][final_svm_model.predict(input_data)[0]]
 
     final_prediction = mode([rf_prediction, nb_prediction, svm_prediction])[0][0]
+    
+    
+    specialist_dict = {
+    'Fungal infection': 'Dermatologist',
+    'Allergy': 'Immunologist',
+    'GERD': 'Gastroenterologist',
+    'Chronic cholestasis': 'Hepatologist',
+    'Drug Reaction': 'Internal Medicine Specialist',
+    'Peptic ulcer disease': 'Gastroenterologist',
+    'AIDS': 'Infectious Disease Specialist',
+    'Diabetes': 'Endocrinologist',
+    'Bronchial Asthma': 'Pulmonologist',
+    'Hypertension': 'Cardiologist',
+    'Migraine': 'Neurologist',
+    'Cervical spondylosis': 'Orthopedic Surgeon',
+    'Paralysis (brain hemorrhage)': 'Neurologist',
+    'Jaundice': 'Gastroenterologist',
+    'Malaria': 'Infectious Disease Specialist',
+    'Chicken pox': 'Dermatologist',
+    'Dengue': 'Infectious Disease Specialist',
+    'Typhoid': 'Internal Medicine Specialist',
+    'hepatitis A': 'Hepatologist',
+    'Hepatitis B': 'Hepatologist',
+    'Hepatitis C': 'Hepatologist',
+    'Hepatitis D': 'Hepatologist',
+    'Hepatitis E': 'Hepatologist',
+    'Alcoholic hepatitis': 'Hepatologist',
+    'Tuberculosis': 'Pulmonologist',
+    'Common Cold': 'General Practitioner',
+    'Pneumonia': 'Pulmonologist',
+    'Dimorphic hemorrhoids(piles)': 'General Surgeon',
+    'Heart attack': 'Cardiologist',
+    'Varicose veins': 'Vascular Surgeon',
+    'Hypothyroidism': 'Endocrinologist',
+    'Hyperthyroidism': 'Endocrinologist',
+    'Hypoglycemia': 'Endocrinologist',
+    'Osteoarthritis': 'Orthopedic Surgeon',
+    'Arthritis': 'Orthopedic Surgeon',
+    '(vertigo) Paroymsal Positional Vertigo': 'Otolaryngologist',
+    'Acne': 'Dermatologist',
+    'Psoriasis': 'Dermatologist',
+    'Impetigo': 'Dermatologist',
+    'Urinary tract infection': 'Urologist'
+}
 
+    specialist_rf = specialist_dict.get(rf_prediction, "Can't specify a specialist.")
+    specialist_nb = specialist_dict.get(nb_prediction, "Can't specify a specialist.")
+    specialist_svm = specialist_dict.get(svm_prediction, "Can't specify a specialist.")
+
+    if(specialist_rf == specialist_nb and specialist_nb == specialist_svm):
+        specialist = specialist_rf
+    elif(specialist_rf == specialist_nb):
+        specialist = specialist_rf
+    elif(specialist_nb == specialist_svm):
+        specialist = specialist_nb
+    elif(specialist_rf == specialist_svm):
+        specialist = specialist_svm
+    else:
+        specialist = "Can't specify a speacialist."
+    
     predictions = {
         "rf_model_prediction": rf_prediction,
         "naive_bayes_prediction": nb_prediction,
         "svm_model_prediction": svm_prediction,
-        "final_prediction":final_prediction
+        "final_prediction":final_prediction,
+        "specialist": specialist
     }
 
     return jsonify(predictions)
